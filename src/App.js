bc4850a95c9306a7be51ec0056785c449a452310
Lucas <lucas.lucenak@gmail.com>
2021-09-15 00:55:35 -0300

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    return (
+        <div className="container">
+            <div className="valid-keys">
+            <span className="matched"></span>
+            <span className="remainder"></span>
+        </div>
+        <div className="typed-keys">asdflucasasdf</div>
+        <div className="completed-words">
+            <ol>
+                <li>arroz</li>
+                <li>feijão</li>
+                <li>batata</li>
+            </ol>
+        </div>
     </div>
-  );
-}
+    );
+};
 
 export default App;
